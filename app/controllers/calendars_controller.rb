4451c6d612bdd5b1dafc17459843c220f0fc364e
shio-san3 <shiofuji47@gmail.com>
2024-07-17 16:03:34 +0900

    Issueの修正（レビュー後一回目）

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,24 +1,41 @@
-<%= form_with model: @plan, url: calendars_path, class: 'form' do |f| %>
-  <%= f.label :date, '日付を選択', class: 'label' %>
-  <%= f.date_field :date, class: 'date-select' %>
-  <%= f.label :plan, '予定を入力', class: 'label' %>
-  <%= f.text_field :plan, class: 'text-input' %>
-  <%= f.submit '保存', class: 'submit-button' %>
-<% end %>
+class CalendarsController < ApplicationController
 
-<div class='calendar'>
-  <% @week_days.each do |day| %>
-    <div class='item'>
-      <div class='date'>
-        <%= day[:month] %>/<%= day[:date] %>
-      </div>
-      <ul class='content'>
-        <% if day[:plans].length != 0 %>
-          <% day[:plans].each do |plan| %>
-            <li class='plan-list'>・<%= plan %></li>
-          <% end %>
-        <% end %>
-      </ul>
-    </div>
-  <% end %>
-</div>
+  # １週間のカレンダー・予定が表示されるページ
+  def index
+    get_week
+    @plan = Plan.new
+  end
+
+  # 予定の保存
+  def create
+    Plan.create(plan_params)
+    redirect_to action: :index
+  end
+
+  private
+
+  def plan_params
+    params.require(:calendars).permit(:date, :plan)
+  end
+
+  def get_week
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+    @todays_date = Date.today
+    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+
+    @week_days = []
+
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
+
+    7.times do |x|
+      today_plans = []
+      plans.each do |plan|
+        today_plans.push(plan.plan) if plan.date == @todays_date + x
+      end
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans }
+      @week_days.push(days)
+    end
+  end
+end
